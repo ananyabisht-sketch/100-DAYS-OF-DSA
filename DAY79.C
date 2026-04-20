51e0e15f4933bd8ab1f03184bfcb9bec50f2b271
Ananya Bisht <ab.ananyabisht@gmail.com>
2026-04-20 09:05:46 +0530

    Add files via upload

---
+++ DAY79.C
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 100
+
+int graph[MAX][MAX];
+int dist[MAX];
+int visited[MAX];
+
+int minDistance(int n) {
+    int min = INT_MAX, minIndex = -1;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i] && dist[i] < min) {
+            min = dist[i];
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+void dijkstra(int n, int source) {
+    for (int i = 1; i <= n; i++) {
+        dist[i] = INT_MAX;
+        visited[i] = 0;
+    }
+
+    dist[source] = 0;
+
+    for (int count = 1; count <= n - 1; count++) {
+        int u = minDistance(n);
+        visited[u] = 1;
+
+        for (int v = 1; v <= n; v++) {
+            if (!visited[v] && graph[u][v] &&
+                dist[u] != INT_MAX &&
+                dist[u] + graph[u][v] < dist[v]) {
+                
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", dist[i]);
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        graph[u][v] = w;
+        graph[v][u] = w; 
+    }
+
+    int source;
+    scanf("%d", &source);
+
+    dijkstra(n, source);
+
+    return 0;
+}
